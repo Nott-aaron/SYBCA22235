5e7ebe989bfbb9b6448d115bdb48c61466aca46b
Spencer Fernandes <spencerfernandes1920@gmail.com>
2024-01-18 09:45:39 +0530

    comittt

--- GitHubProject/GitHubDemo/src/Demo/Add.java
+++ GitHubProject/GitHubDemo/src/Demo/Add.java
@@ -6,12 +6,12 @@ public class Add {
 
 	public static void main(String[] args) {
 
-		int x,y,sum;
+		int a,b,sum;
 		System.out.println("Enter two numbers");
 		Scanner scan = new Scanner(System.in);
-		x=scan.nextInt();
-		y=scan.nextInt();
-		sum=x+y;
+		a=scan.nextInt();
+		b=scan.nextInt();
+		sum=a+b;
 		System.out.println("The sum is "+sum);
 
 		
